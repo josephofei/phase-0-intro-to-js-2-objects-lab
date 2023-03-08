97a90471185773ab44943be09ddd8640dffc2586
Joseph Ofei <josephofei24@gmail.com>
2023-03-07 20:20:31 -0500

    addss object lab complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+const employee = { name: 'joseph', streetAddress: '225 winfield st' };
+
+function updateEmployeeWithKeyAndValue(obj, key, val) {
+    const newObj = { ...obj };
+    newObj[key] = val;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, val) {
+    obj[key] = val;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = { ...obj };
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+    delete obj[key];
+    return obj;
+}
